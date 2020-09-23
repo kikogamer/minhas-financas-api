fd32c24492f9c4fc025e01b80e64f5d29dddda6b
Ronaldo <kikogamer@gmail.com>
2020-09-23 19:42:45 -0300

    user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_params) { attributes_for(:user) }
+
+  it 'is valid with name, email and password' do
+    expect(create(:user)).to be_valid
+  end
+
+  it 'is invalid password with length smaller than 7' do
+    user_params[:password] = FFaker::Internet.password(1, 7)
+    expect(User.create(user_params)).to_not be_valid
+  end
+
+  it 'is invalid password with length greater than 72' do
+    user_params[:password] = FFaker::Internet.password(73, 999)
+    expect(User.create(user_params)).to_not be_valid
+  end
+
+  it 'is invalid without name' do
+    user_params[:name] = nil
+    expect(User.create(user_params)).to_not be_valid
+  end
+
+  it 'is invalid without email' do
+    user_params[:email] = nil
+    expect(User.create(user_params)).to_not be_valid
+  end
+
+  it 'is invalid without password' do
+    user_params[:password] = nil
+    expect(User.create(user_params)).to_not be_valid
+  end
 end
