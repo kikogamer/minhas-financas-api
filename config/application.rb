f24144a9fde07be27390a8437f69010019aec6e7
Ronaldo <kikogamer@gmail.com>
2020-10-07 16:51:10 -0300

    allow environment variables

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,14 @@ module MinhasFinancasApi
 
     config.middleware.use Rack::Attack
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration can go into files in config/initializers
